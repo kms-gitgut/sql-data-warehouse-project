6edf75116376218002251136626b6e1dd6194851
Kamil Szafranski <kamil.m.szafranski@gmail.com>
2026-02-03 12:31:58 +0100

    Update proc_load_silver.sql

--- scripts/silver/proc_load_silver.sql
+++ scripts/silver/proc_load_silver.sql
@@ -16,7 +16,7 @@ None.
 This stored procedure does not accept any parameters or return any values
 
 Usage Example:
-EXEC Silver.load_silver;
+EXEC silver.load_silver;
 	
 WARNING:
     Running this script will truncate all tables in the database silver layer if they exist. 
