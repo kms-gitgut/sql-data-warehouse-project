0062529e9419c3b932dc9cd0e6fb79cf8138e38d
Kamil Szafranski <kamil.m.szafranski@gmail.com>
2026-02-03 13:51:48 +0100

    Update proc_load_bronze.sql

--- scripts/bronze/proc_load_bronze.sql
+++ scripts/bronze/proc_load_bronze.sql
@@ -48,7 +48,7 @@ CREATE OR ALTER PROCEDURE bronze.load_bronze AS
           PRINT '>> Inserting Data Into Table: bronze.crm_cust_info'; 
           -- Inserts the source data into the table 
           BULK INSERT bronze.crm_cust_info
-          FROM 'C:\Users\Ania\Desktop\Chłop\SQL\sql-data-warehouse-project\datasets\source_crm\cust_info.csv'
+          FROM 'C:\Users\Ania\Desktop\Chłop\SQL\DataFromBaraa\sql-data-warehouse-project\datasets\source_crm\cust_info.csv'
           WITH
           (
               FIRSTROW = 2,
@@ -66,7 +66,7 @@ CREATE OR ALTER PROCEDURE bronze.load_bronze AS
       
           PRINT '>> Inserting Data Into Table: bronze.crm_prd_info'; 
           BULK INSERT bronze.crm_prd_info
-          FROM 'C:\Users\Ania\Desktop\Chłop\SQL\sql-data-warehouse-project\datasets\source_crm\prd_info.csv'
+          FROM 'C:\Users\Ania\Desktop\Chłop\SQL\DataFromBaraa\sql-data-warehouse-project\datasets\source_crm\prd_info.csv'
           WITH
           (
               FIRSTROW = 2,
@@ -84,7 +84,7 @@ CREATE OR ALTER PROCEDURE bronze.load_bronze AS
   
           PRINT '>> Inserting Data Into Table: bronze.crm_sales_details'; 
           BULK INSERT bronze.crm_sales_details
-          FROM 'C:\Users\Ania\Desktop\Chłop\SQL\sql-data-warehouse-project\datasets\source_crm\sales_details.csv'
+          FROM 'C:\Users\Ania\Desktop\Chłop\SQL\DataFromBaraa\sql-data-warehouse-project\datasets\source_crm\sales_details.csv'
           WITH
           (
               FIRSTROW = 2,
@@ -107,7 +107,7 @@ CREATE OR ALTER PROCEDURE bronze.load_bronze AS
   
           PRINT '>> Inserting Data Into Table: bronze.erp_cust_az12'; 
           BULK INSERT bronze.erp_cust_az12
-          FROM 'C:\Users\Ania\Desktop\Chłop\SQL\sql-data-warehouse-project\datasets\source_erp\cust_az12.csv'
+          FROM 'C:\Users\Ania\Desktop\Chłop\SQL\DataFromBaraa\sql-data-warehouse-project\datasets\source_erp\cust_az12.csv'
           WITH
           (
               FIRSTROW = 2,
@@ -125,7 +125,7 @@ CREATE OR ALTER PROCEDURE bronze.load_bronze AS
   
           PRINT '>> Inserting Data Into Table: bronze.erp_loc_a101'; 
           BULK INSERT bronze.erp_loc_a101
-          FROM 'C:\Users\Ania\Desktop\Chłop\SQL\sql-data-warehouse-project\datasets\source_erp\loc_a101.csv'
+          FROM 'C:\Users\Ania\Desktop\Chłop\SQL\DataFromBaraa\sql-data-warehouse-project\datasets\source_erp\loc_a101.csv'
           WITH
           (
               FIRSTROW = 2,
@@ -143,7 +143,7 @@ CREATE OR ALTER PROCEDURE bronze.load_bronze AS
   
           PRINT '>> Inserting Data Into Table: bronze.erp_px_cat_g1v2'; 
           BULK INSERT bronze.erp_px_cat_g1v2
-          FROM 'C:\Users\Ania\Desktop\Chłop\SQL\sql-data-warehouse-project\datasets\source_erp\px_cat_g1v2.csv'
+          FROM 'C:\Users\Ania\Desktop\Chłop\SQL\DataFromBaraa\sql-data-warehouse-project\datasets\source_erp\px_cat_g1v2.csv'
           WITH
           (
               FIRSTROW = 2,
