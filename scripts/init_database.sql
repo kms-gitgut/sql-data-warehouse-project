0e1f7539882ceb74fa93550b8251129327bc764a
Kamil Szafranski <kamil.m.szafranski@gmail.com>
2026-02-06 18:09:14 +0100

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -11,6 +11,7 @@ WARNING:
     Running this script will drop the entire 'DataWarehouse' database if it exists. 
     All data in the database will be permanently deleted. Proceed with caution 
     and ensure you have proper backups before running this script.
+=============================================================
 */
 
 USE master;
